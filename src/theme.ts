c072208c99b5a95ef90245bf2060c81fd6d73199
kyawThetHtun <kyawthe235@gmail.com>
2024-08-24 00:50:03 +0630

    dark mode button

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendBaseTheme, ThemeConfig } from "@chakra-ui/react";
+
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark'
+};
+
+const theme = extendBaseTheme({ config });
+
+export default theme;
