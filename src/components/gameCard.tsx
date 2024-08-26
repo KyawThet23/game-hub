e3e71aebe7d3088f84fa5c20b41a89cf604010e7
kyawThetHtun <kyawthe235@gmail.com>
2024-08-27 00:10:47 +0630

    add games icons

--- src/components/gameCard.tsx
+++ src/components/gameCard.tsx
@@ -1,21 +1,25 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
-import { Game } from "../hooks/useGames"
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import PlatformIcons from "./platformIcons";
 
 interface Props {
-  game : Game
+  game: Game;
 }
 
-const GameCard = ({ game } : Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <div>
       <Card borderRadius={10} overflow={"hidden"}>
         <Image src={game.background_image} />
         <CardBody>
-          <Heading fontSize='2xl'>{game.name}</Heading>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <PlatformIcons
+            platform={game.parent_platforms.map((p) => p.platform)}
+          />
         </CardBody>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
