d9a76339923b81d654a64b9efa0a930a80145ff4
kyawThetHtun <kyawthe235@gmail.com>
2024-09-02 23:57:00 +0630

    fixing UI bug

--- src/components/genreList.tsx
+++ src/components/genreList.tsx
@@ -18,8 +18,8 @@ const GenreList = ({ onSelectGenre , selectedGenre }:Props) => {
       {datas.map((genre) => (
         <ListItem key={genre.id} paddingY='5px'>
           <HStack>
-            <Image boxSize='32px' borderRadius={8} src={ getCroppedImageUrl(genre.image_background)} />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg'>{genre.name}</Button>
+            <Image objectFit='cover' boxSize='32px' borderRadius={8} src={ getCroppedImageUrl(genre.image_background)} />
+            <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg'>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
